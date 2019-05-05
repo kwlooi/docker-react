272c5c07e06a11256803460e5095f833858785e0
Looi, Kok-Wai (kwlooi) <kwlooi@gmail.com>
2019-05-05 16:14:25 -0500

    Update to App.js for a new message

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React - this is fun
           </a>
         </header>
       </div>
